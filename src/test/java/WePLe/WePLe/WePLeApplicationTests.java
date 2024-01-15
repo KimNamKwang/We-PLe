fba2a4cffde5725e55289a033d2ce160ce5a7e12
KimNamKwang <durante0916@gmail.com>
2024-01-15 23:03:53 +0900

    [24.01.15] first commit

---
+++ src/test/java/WePLe/WePLe/WePLeApplicationTests.java
@@ -0,0 +1,13 @@
+package WePLe.WePLe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WePLeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
