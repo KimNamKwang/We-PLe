2cdb4a790229afc3b9c414fc9754b50d0cf8d349
KimNamKwang <durante0916@gmail.com>
2024-01-17 23:41:30 +0900

    [24.01.17] 체인지

--- src/main/java/WePLe/WePLe/controllers/MainController.java
+++ src/main/java/WePLe/WePLe/controllers/MainController.java
@@ -11,6 +11,24 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value = "/main")
+
 public class MainController {
 
+    @Autowired
+
+    @RequestMapping(value = "/welcome", method = RequestMethod.GET)
+    public ModelAndView main(@RequestParam Map<String, Object> params,
+            ModelAndView modelAndView) {
+        Object resultMap = params;
+        return modelAndView;
+    }
 }
